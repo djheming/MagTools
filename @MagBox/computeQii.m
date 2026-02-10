5088fb1d0083e21669803fa6222e652da7800d6d
Doug Hemingway <douglas.hemingway@gmail.com>
2026-02-10 13:12:15 -0600

    Massive restructuring to improve transparency and robustness with regard to edge cases.

--- @MagBox/computeQii.m
+++ @MagBox/computeQii.m
@@ -38,32 +38,72 @@ alphas = nan(2,N);
 % is semi-infinite in either the j or k directions, the angle becomes:
 % arctan( sign(rjb) * rkc/ria ) where the sign is important because the
 % limit of the usual expression as rjb goes to ±infinnity retains the
-% sign of rjb. Also, note that we don't have to do anything special if
+% sign of rjb. If the prism is semi-infinite in both j and k, the corner
+% contributes a ±pi/2, with the sign depending on the signs of rjb and rkc.
+% Also, note that we don't have to do anything special if
 % the prism is semi-infinite in the i direction because the ria in the
 % denominator will just push the corresponding angle towards zero.
+% Finally, if ria is zero, the corner contributes a ±pi/2 with the sign
+% depending on the signs of rjb and rkc.
 for a = [ 1 2 ]
     for b = [ 1 2 ]
         for c = [ 1 2 ]
+
+            % Shorthand for logical indexing.
+            ria = ri(a,:);
+            rjb = rj(b,:);
+            rkc = rk(c,:);
+            infia = isinf(ria);
+            infjb = isinf(rjb);
+            infkc = isinf(rkc);
+
+            % Compute sign corresponding to this corner's angle.
             s_abc = (-1)^(a+b+c);
-            if isinf(ri(a,1))
-                thetas(a,b,c,:) = 0;
-            elseif isinf(rj(b,1))
-                thetas(a,b,c,:) = s_abc * sign(rj(b,1)) * atan( rk(c,:)./ri(a,:) );
-            elseif isinf(rk(c,1))
-                thetas(a,b,c,:) = s_abc * sign(rk(c,1)) * atan( rj(b,:)./ri(a,:) );
-            else
-                Rabc = sqrt( ri(a,:).^2 + rj(b,:).^2 + rk(c,:).^2 );
-                thetas(a,b,c,:) = s_abc * atan( (rj(b,:).*rk(c,:)) ./ (ri(a,:).*Rabc) );
+
+            % Case 0: We are viewing from the plane of the ia face.
+            % Here, each corner contributes a constant of ±pi/2, depending
+            % on the signs of rjb and rkc terms.
+            riaiszero = (ria==0);
+            if any(riaiszero)
+                thetas(a,b,c,riaiszero) = s_abc * sign(rjb(riaiszero)) .* sign(rkc(riaiszero)) * pi/2;
             end
+
+            % Case 1: Entirely finite.
+            inds1 = ~riaiszero & ~infia & ~infjb & ~infkc;
+            if any(inds1)
+                Rabc = sqrt( ria.^2 + rjb.^2 + rkc.^2 );
+                thetas(a,b,c,inds1) = s_abc * atan( (rjb(inds1).*rkc(inds1)) ./ (ria(inds1).*Rabc(inds1)) );
+            end
+
+            % Case 2: The ia face is infinitely far away.
+            inds2 = ~riaiszero & infia;
+            if any(inds2)
+                thetas(a,b,c,inds2) = 0;
+            end
+
+            % Case 3.1: Only the jb face is infinitely far away.
+            inds31 = ~riaiszero & ~infia & infjb & ~infkc;
+            if any(inds31)
+                thetas(a,b,c,inds31) = s_abc * sign(rjb(inds31)) .* atan( rkc(inds31)./ria(inds31) );
+            end
+
+            % Case 3.2: Only the kc face is infinitely far away.
+            inds32 = ~riaiszero & ~infia & ~infjb & infkc;
+            if any(inds32)
+                thetas(a,b,c,inds32) = s_abc * sign(rkc(inds32)) .* atan( rjb(inds32)./ria(inds32) );
+            end
+
+            % Case 3.3: The jb and kc faces are both infinitely far away.
+            % This is the quarter-plane limit, which is ±pi/2, depending on
+            % the signs of ria, rjb, rkc terms.
+            inds33 = ~riaiszero & ~infia & infjb & infkc;
+            if any(inds33)
+                thetas(a,b,c,inds33) = s_abc * sign(rjb(inds33).*rkc(inds33)./ria(inds33)) * pi/2;
+            end
+
         end
     end
 end
-
-% If there are any evaluation points collinear with the edge of a
-% semi-infinite prism, we'll have hit some 0/0 divisions. In these cases,
-% the angles should really be zero.
-badthetas = isnan(thetas);
-thetas(badthetas) = 0;
 
 % Sum across thetas to get alphas.
 alphas(1,:) = sum(thetas(1,:,:,:),[ 2 3 ]);
--- @MagBox/computeQij.m
+++ @MagBox/computeQij.m
@@ -1,4 +1,4 @@
-function [ Qij, f_ab, f_c, ts, Rsq ] = computeQij( ri, rj, rk )
+function [ Qij, f_ab, rho ] = computeQij( ri, rj, rk )
 
 % Qij = computeQij( ri, rj, rk )
 % 
@@ -29,101 +29,152 @@ function [ Qij, f_ab, f_c, ts, Rsq ] = computeQij( ri, rj, rk )
 
 % Initialize.
 N = size(ri,2);
-ts = nan(2,2,2,N);
+rho = nan(2,2,N);
 f_ab = nan(2,2,N);
-f_c = nan(2,N);
-Rsq = nan(2,2,N);
+Qij_ab = nan(2,2,N);
 
-% Perform summation over all 8 vertices. For a finite prism, we
-% can then calculate the f_ab ratios and then Qij from there.
-% If any of the i or j faces are at infinity, then we can't
-% calculate all the f_ab ratios from the ts and instead have to
-% calculate some of them directly. If any of the k faces are at
-% infinity, we can't use the f_ab ratios and we have to
-% calculate the f_c ratios instead.
-if ~any( isinf(rk(:,1)) )
-    
-    % Here, the prism is fully finite in the k-direction. If
-    % any of the i or j faces is at infinity, we already know
-    % some of the corresponding f_ab ratios.
-    for a = [ 1 2 ]
-        if isinf(ri(a,1))
-            f_ab(a,:,:) = 1;
-        else
-            for b = [ 1 2 ]
-                if isinf(rj(b,1))
-                    f_ab(:,b,:) = 1;
-                else
-                    for c = [ 1 2 ]
-                        Rabc = sqrt( ri(a,:).^2 + rj(b,:).^2 + rk(c,:).^2 );
-                        ts(a,b,c,:) = rk(c,:) + Rabc;
-                    end
-                    f_ab(a,b,:) = ts(a,b,1,:) ./ ts(a,b,2,:);
-                end
-            end
-        end
+% Shorthand for clarity and to aid with logical indexing.
+rk1 = rk(1,:);
+rk2 = rk(2,:);
+infk1 = isinf(rk1);
+infk2 = isinf(rk2);
+infk = infk1 | infk2;
+isAbove = rk1>0 & rk2>0;
+isBelow = rk1<0 & rk2<0;
+
+% Compute the lateral (ij) hypotenuese terms for each of the four edges.
+s = nan(2,2); % For keeping track of the signs of each edge term.
+for a = [ 1 2 ]
+    for b = [ 1 2 ]
+        rho(a,b,:) = hypot( ri(a,:), rj(b,:) );      
+        s(a,b) = (-1)^(a+b);
     end
+end
 
-    % We could compute Qij from the f_ab ratios with
-    % Qij = log( (f_ab(1,1,:).*f_ab(2,2,:))./(f_ab(1,2,:).*f_ab(2,1,:)) ); 
-    % but this does not handle singularities properly. Instead, we'll start
-    % from the ts terms. When the evaluation point is aligned with the ij
-    % edge of the prism, and if rkc<0, the corresponding ts is zero and so
-    % its logarithm will be -Infinity. Outside the prism, these always come
-    % in cancelling pairs, so we can simply replace those log(ts) terms
-    % with zero.
-    logts = log(ts);
-    ts0inds = isinf(logts);
-    logts(ts0inds) = 0;
-    [as, bs, cs] = ndgrid(1:2, 1:2, 1:2);
-    S = repmat( -(-1).^(as + bs + cs), 1, 1, 1, N ); % Places correct sign in front of logarithm terms.
-    A15terms = S .* logts; % A15 refers to the equation in the paper.
-    Qij = sum( A15terms, [ 1 2 3 ] ); 
-    
-    % Reshape to get the desired 1x1xN layout.
-    Qij = reshape( Qij, 1, 1, [] );
+% Loop over the four ij edges (pairing up the terms along the k dimension).
+for a = [ 1 2 ]
+    for b = [ 1 2 ]
 
-else
+        % Rather than work on each corner in isolation, we pair the
+        % corners that are separated in the k-direction. That is, we will
+        % go to work on each of the four edges, which are index by (a,b).
+        % We do this to enable better handling of singularities where the
+        % evaluation point is collinear with one of the edges or where the
+        % prism has infinite or semi-infinite extent. 
 
-    % Here, the prism has one or both of its k-faces at
-    % infinity. We'll need to use f_c here instead of the
-    % f_ab ratios.
-    for c = [ 1 2 ]
-        if ~isinf(rk(c,1))
-            % This is the finite k-face. Calcluate the ts in
-            % the normal way and use this to make f_c.
-            for a = [ 1 2 ]
-                for b = [ 1 2 ]
-                    Rabc = sqrt( ri(a,:).^2 + rj(b,:).^2 + rk(c,:).^2 );
-                    ts(a,b,c,:) = rk(c,:) + Rabc;
-                end
-            end
-            f_c(c,:) = ( ts(1,1,c,:).*ts(2,2,c,:) )./( ts(1,2,c,:).*ts(2,1,c,:) );
-        elseif rk(c,1) > 0
-            % This is the case where rk1 goes to +infinity. In that case,
-            % all the ts converge to some large value such that f1 = 1.
-            f_c(c,:) = 1;
-        elseif rk(c,1) < 0
-            % This is the case where rk2 goes to -infinity. In that case,
-            % each of the ts shrink towards zero (but do not converge) in
-            % such a way that f2 approaches a finite number related to the
-            % cross-sectional distances to each of the prism's four finite
-            % edges.
-            for a = [ 1 2 ]
-                for b = [ 1 2 ]
-                    Rsq(a,b,:) = ri(a,:).^2 + rj(b,:).^2;
-                end
-            end
-            f_c(c,:) = ( Rsq(1,1,:).*Rsq(2,2,:) )./( Rsq(1,2,:).*Rsq(2,1,:) );
+        % Initialize this edge term.
+        edgeTerm = zeros(1,N);
+
+        % Extract the relevant rho and reshape for convenience in logical indexing.
+        rho_ab = reshape(rho(a,b,:),1,N);
+        rhoiszero = (rho_ab==0); % Points that are collinear with edge ab.
+        rhoisinf = isinf(rho_ab); % This edge is infinitely far away.
+        okrho = ~rhoiszero & ~rhoisinf; % Finite rho.
+
+        % Branch 0: Evaluation points with infinite rho. This occurs when
+        % the prism is semi-infinite in either i or j. In these cases,
+        % edge ab contributes nothing to the field and so we do nothing
+        % (because the edgeTerm has already been initialized to zero). 
+        % One note of caution: In cases where the prism has semi-infinite
+        % or infinite extent in the k-direction, the edgeTerms will contain
+        % infinities that have been dropped from the Branch 1 calculations
+        % below. We can only drop them because they generally cancel due to
+        % the alternating signs in the summation over (a,b). The one
+        % exception to this is when the prism is semi-infinite in BOTH 
+        % i and j, in which case three of the four rho(a,b) terms will be
+        % zero, leaving only one edge to contribute to Qij. If this edge is
+        % missing a top or bottom, there will be a dangling infinity that
+        % does not cancel out. We will handle this special case at the very
+        % end (see below).
+
+        % Branch 1: Evaluation points with finite rho.
+        if any(okrho)
+
+            % Case 1.0: Edge is entirely finite. 
+            inds_1_0 = okrho & ~infk1 & ~infk2;
+            edgeTerm(inds_1_0) = asinh(rk1(inds_1_0)./rho_ab(inds_1_0)) - asinh(rk2(inds_1_0)./rho_ab(inds_1_0));
+
+            % Case 1.1: Semi-infinite with rk1=+inf, rk2 finite. The prism has no bottom.
+            inds_1_1 = okrho & (infk1 & rk1>0) & ~infk2;
+            edgeTerm(inds_1_1) = - asinh(rk2(inds_1_1)./rho_ab(inds_1_1)) - log(rho_ab(inds_1_1));
+
+            % Case 1.2: Semi-infinite with rk1 finite, rk2=-inf. The prism has no top.
+            inds_1_2 = okrho & ~infk1 & (infk2 & rk2<0);
+            edgeTerm(inds_1_2) = asinh(rk1(inds_1_2)./rho_ab(inds_1_2)) - log(rho_ab(inds_1_2));
+
+            % Case 1.3: Edge is infinite (rk1=+inf, rk2=-inf). Prism has no
+            % top or bottom. This is the 2D case.
+            inds_1_3 = okrho & (infk1 & rk1>0) & (infk2 & rk2<0);
+            edgeTerm(inds_1_3) = -2 * log(rho_ab(inds_1_3));
+
         end
-    end
 
-    % Compute Qij from the f_c ratios.
-    Qij = log( f_c(1,:)./f_c(2,:) );
-    
-    % This will be a 1xN array. But we want 1x1xN (since the
-    % full Q is 3x3xN). To fix this, we can permute the
-    % dimensions.
-    Qij = permute( Qij, [ 1 3 2 ] );
+        % Branch 2: Evaluation points where rho-->0 (singularities).
+        if any(rhoiszero)
+
+            % Case 2.0: We are touching the edge.
+            inds_2_0 = rhoiszero & ~isAbove & ~isBelow;
+            edgeTerm(inds_2_0) = Inf;
+
+            % Case 2.1: Edge is entirely finite and we are above or below
+            % (not touching).
+            inds_2_1 = rhoiszero & ~infk & ( isAbove | isBelow );
+            edgeTerm(inds_2_1) = sign(rk1(inds_2_1)) .* log(rk1(inds_2_1)./rk2(inds_2_1));
+
+            % Case 2.2: Prism has no bottom and we are above.
+            inds_2_2 = rhoiszero & infk1 & isAbove;
+            edgeTerm(inds_2_2) = - log(2*abs(rk2(inds_2_2)));
+
+            % Case 2.3: Prism has no top and we are below.
+            inds_2_3 = rhoiszero & infk2 & isBelow;
+            edgeTerm(inds_2_3) = - log(2*abs(rk1(inds_2_3)));
+
+        end
+
+        % Apply the approriate sign for this term.
+        Qij_ab(a,b,:) = s(a,b) * edgeTerm;
+        f_ab(a,b,:) = exp(edgeTerm);
+
+    end
 
 end
+
+% Sum the terms across a and b.
+Qij = sum( Qij_ab, [ 1 2 ] );
+
+% Final check: make sure we are not violating our assumption about the rk1
+% and rk2 infinities cancelling. They will generally cancel because we are
+% summing (with alternating signs) across the four edges. However, if we
+% have a prism that is semi-infinite in both the i and j directions, then
+% three of our four edges will be contributing nothing to the summation for
+% Qij. This means that if the remaining edge (the only contributing edge)
+% contains an infinity, there is nothing to cancel it out. We only need to
+% look at points where rk1 or rk2 are infinite.
+if any(infk)
+
+    % Here, we have a prism that goes to infinity in rk1 and/or rk2. 
+    % Now we just need to see how many of the corresponding rhos are also
+    % infinite. If it's an even number (0 or 2), we're good because the
+    % corresponding infinite rkc terms will cancel. On the other hand, if
+    % there is an odd number of rhos that have gone to infinity, we'll be
+    % left with a dangling rkc infinity that doesn't cancel. 
+    
+    % Track the edges that have finite rhos and note their signs.
+    sfrho = s .* ~isinf(rho); 
+
+    % For each evaluation point, the sum of sfrho will be zero if all the
+    % infinities cancel. If it's not zero, its sign tells us the sign of the
+    % infinity (i.e., if the rho(a,b) term is finite, the infinity is
+    % positive for even a+b and negative for odd a+b).
+    inf_sign = reshape(sum(sfrho,[1 2]),1,N);
+    badinf = infk & (inf_sign~=0);
+    if any(badinf)
+        Qij(badinf) = inf_sign(badinf) * Inf;
+    end
+    
+end
+
+
+
+
+
